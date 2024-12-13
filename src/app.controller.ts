c0d702d24bac39d62b014e33f0e9f677084163ce
Giuseppe Lanna <giuseppe@orium.network>
2024-12-13 18:28:30 -0300

    add reup ticks

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,33 @@
-import { Controller, Get, ImATeapotException } from '@nestjs/common'
+import { Controller, Get, ImATeapotException, Logger } from '@nestjs/common'
 import { ApiOperation } from '@nestjs/swagger'
 
 const Package = require('../package.json')
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name)
+
+  constructor() {
+    const logger = this.logger
+    const backend_url = process.env.MAGIC3T_BACKEND_URL
+    const reup_rate = Number.parseInt(process.env.REUP_RATE)
+
+    async function tick() {
+      const initialTime = Date.now()
+
+      if (!backend_url) return
+      const response = await fetch(`${backend_url}/status`)
+      const body = await response.json()
+
+      logger.debug(`Received status ${body.status} from tick.`)
+
+      const delta = Date.now() - initialTime
+      setTimeout(tick, Math.max(reup_rate - delta, 0))
+    }
+
+    tick()
+  }
+
   @Get('teapot')
   teapot(): never {
     throw new ImATeapotException()
--- src/match/match.controller.ts
+++ src/match/match.controller.ts
@@ -1,22 +1,35 @@
-import { Controller, HttpCode, Post } from '@nestjs/common'
+import { Controller, Get, HttpCode, Post, UseGuards } from '@nestjs/common'
 
 import { MatchService } from './services'
 import { CurrentMatchAdapter } from './decorators'
 import { MatchSideAdapter } from './types'
 import { ApiOperation } from '@nestjs/swagger'
+import { MatchGuard } from './match.guard'
+import { AuthGuard } from '@/auth/auth.guard'
 
+@UseGuards(AuthGuard)
 @Controller('match')
 export class MatchController {
   constructor(private matchService: MatchService) {}
 
   @ApiOperation({
     summary: 'Forfeit',
-    description: 'Forfeits the current match',
+    description: 'Forfeit the current match',
   })
-  @Post('forfeit')
+  @Post(':matchId/forfeit')
   @HttpCode(200)
+  @UseGuards(MatchGuard)
   handleForfeit(@CurrentMatchAdapter() matchAdapter: MatchSideAdapter) {
     matchAdapter.forfeit()
+  }
+
+  @ApiOperation({
+    summary: 'Get state',
+    description: 'Get the state of the current match',
+  })
+  @Get('state')
+  getState() {
+    return 1
   }
 
   // @Get('matchId')
