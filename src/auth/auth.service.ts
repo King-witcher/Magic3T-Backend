7f86aee150cee15a22a643cb3cb084b013e0eacc
Giuseppe Lanna <giuseppe@orium.network>
2025-01-28 17:07:10 -0300

    add getAllUsers to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,22 @@
 import { FirebaseService } from '@/firebase'
 import { Injectable } from '@nestjs/common'
+import { UserRecord } from 'firebase-admin/auth'
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly firebaseService: FirebaseService) {}
+  constructor(private firebaseService: FirebaseService) {}
 
   async validateToken(token: string): Promise<string> {
     const decoded = await this.firebaseService.firebaseAuth.verifyIdToken(token)
     return decoded.uid
+  }
+
+  /**
+   * Gets all user records from Firebase Auth, up to 1000.
+   */
+  async getAllUsers(nextPageToken?: string): Promise<[UserRecord[], string?]> {
+    const firebaseAuth = this.firebaseService.firebaseAuth
+    const listResult = await firebaseAuth.listUsers(1000, nextPageToken)
+    return [listResult.users, listResult.pageToken]
   }
 }
