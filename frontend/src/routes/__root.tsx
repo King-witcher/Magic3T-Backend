904488c05d7a23219186d60c3ef8bec8feef57f2
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2026-01-14 17:32:16 -0300

    fix tanstack router devtools

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -6,22 +6,10 @@ import {
 } from '@/components/templates'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
-import { SpeedInsights as VercelSpeedInsights } from '@vercel/speed-insights/react'
-import React from 'react'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { Providers } from './-providers'
-
-const TanStackRouterDevtools =
-  import.meta.env.NODE_ENV === 'production'
-    ? () => null // Render nothing in production
-    : React.lazy(() =>
-        // Lazy load in development
-        import('@tanstack/router-devtools').then((res) => ({
-          default: res.TanStackRouterDevtools,
-          // For Embedded Mode
-          // default: res.TanStackRouterDevtoolsPanel
-        }))
-      )
 
 export const Route = createRootRoute({
   component: () => (
@@ -30,8 +18,8 @@ export const Route = createRootRoute({
         <Outlet />
         <ConsoleTab />
       </RootLayout>
-      <VercelAnalytics />
-      <VercelSpeedInsights />
+      <Analytics />
+      <SpeedInsights />
       <ReactQueryDevtools />
       <TanStackRouterDevtools position="bottom-left" />
     </Providers>
